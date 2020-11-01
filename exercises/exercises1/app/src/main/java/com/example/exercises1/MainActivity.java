85068226165f4e04e51c09e4ac50d09f2ea1e297
longzhishanhe <longzhishanhe@163.com>
2020-11-01 23:14:38 +0800

    练习1

---
+++ exercises/exercises1/app/src/main/java/com/example/exercises1/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.exercises1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button hint=findViewById(R.id.hint);
+        Button register=findViewById(R.id.register);
+
+        hint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog aldg;
+                AlertDialog.Builder adBd=new AlertDialog.Builder(MainActivity.this);
+                adBd.setTitle("提示对话框");
+                adBd.setMessage("请登录");
+                adBd.setPositiveButton("关闭", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                aldg=adBd.create();
+                aldg.show();
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
